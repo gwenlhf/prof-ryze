45576a0ed2ae2a0f97528406aff72365a8cdf5a1
Ethan David <ead900@gmail.com>
2018-05-14 18:40:34 -0400

    it works!

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
+'use strict';
+
 const functions = require('firebase-functions');
+const { WebhookClient, Card, Suggestion } = require('dialogflow-fulfillment');
 const firebase = require('firebase');
 const fbConfig = require('./nocommit/prof-ryze-firebase-usercfg.json');
 
@@ -12,42 +15,52 @@ let db = firebase.database();
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(( req, res ) => webHookDriver( req, res ));
 
 function webHookDriver( req, res ) {
-	let hook = req.body;
-	let intent = hook.queryResult.intent;
-	let params = hook.queryResult.parameters;
-	let result;
-	if ( intent.displayName === 'Ability-Desc' ) {
-		result = abilityDescHandler(params)
-			.then(data => abilityDescMsg(data));
-	}
-	result.then(body => res.send(body))
-		.catch(err => res.send(err));
+	let agent = new WebhookClient({request: req, response: res});
+	let intentMap = new Map();
+	intentMap.set('Ability-Desc', abilityDescHandler);
+	intentMap.set('Champ-Tip-Ally', a => champTipHandler( a, true));
+	intentMap.set('Champ-Tip-Enemy', a => champTipHandler( a, false));
+	agent.handleRequest(intentMap);
 }
 
-function abilityDescHandler ( params ) {
-	let key = params.Champion;
+function champTipHandler( agent, isAlly=true ) {
+	let key = agent.parameters.Champion;
+	return fetchChampTip( key, isAlly )
+		.then( tip => champTipResponses( agent, tip, isAlly ));
+}
 
-	let result;
-	switch ( params.Ability ) {
+function champTipResponses ( agent, tip, isAlly ) {
+	agent.add( tip );
+	let card = (isAlly) ? new Card(`Playing against `)
+	if (isAlly) {
+
+	}
+}
+
+function abilityDescHandler ( agent ) {
+	let key = agent.parameters.Champion;
+	if (agent.parameters.Ability === 'Passive') {
+		return fetchChampPassive( key )
+			.then(passive => passiveDescResponses(agent, passive));
+	}
+	let idx = 0;
+	switch (agent.parameters.Ability) {
 	case 'Q':
-		result = fetchChampAbility( key, 0 );
 		break;
 	case 'W':
-		result = fetchChampAbility( key, 1 );
+		idx = 1;
 		break;
 	case 'E':
-		result = fetchChampAbility( key, 2 );
+		idx = 2;
 		break;
 	case 'R':
-		result = fetchChampAbility( key, 3 );
+		idx = 3;
 		break;
-	case 'Passive':
-		return fetchChampPassive( key )
-			.then(passive => passiveDescMsg(passive));
 	default:
-		return Promise.reject(new Error("No ability specified"));
+		return Promise.reject(new Error("invalid Ability parameter"));
 	}
-	return result.then(ability => abilityDescMsg(ability));
+	return fetchChampAbility( key, idx )
+		.then(ability => abilityDescResponses(agent, ability));
 }
 
 function retrieveDb( ref ) {
@@ -56,37 +69,23 @@ function retrieveDb( ref ) {
 		.then(snapshot => snapshot.val());
 }
 
-function passiveDescMsg( passive ) {
-	return {
-		fulfillmentText : passive.sanitizedDescription,
-		fulfillmentMessages : [
-			{
-				card : {
-					title : passive.name,
-					subtitle : passive.sanitizedDescription,
-					imageUri : ddragon + '/img/passive/' + passive.image.full
-				}
-			}
-		]
-	};
+function passiveDescResponses( agent, passive ) {
+	agent.add( `${ passive.name }: ${ passive.sanitizedDescription }` );
+	agent.add( new Card({
+		title : passive.name,
+		text : passive.sanitizedDescription,
+		imageUrl : ddragon + '/img/passive/' + passive.image.full
+	}));
 }
 
-function abilityDescMsg( ability ) {
-	return {
-		fulfillmentText: ability.sanitizedDescription,
-		fulfillmentMessages: [
-			{
-				card : {
-					title : ability.name,
-					subtitle : ability.sanitizedDescription,
-					imageUri : ddragon + '/img/spell/' + ability.image.full
-				}
-			}
-		]
-	};
+function abilityDescResponses( agent, ability ) {
+	agent.add( `${ ability.name }: ${ ability.sanitizedDescription }` );
+	agent.add( new Card({
+		title : ability.name,
+		text : ability.sanitizedDescription,
+		imageUrl : ddragon + '/img/spell/' + ability.image.full
+	}));
 }
-
-
 
 function fetchAbilityDesc( key, idx=0 ) {
 	return retrieveDb(`/champs/data/${ key }/spells/${ idx }/sanitizedDescription`);
@@ -100,7 +99,20 @@ function fetchChampPassive( key ) {
 	return retrieveDb(`/champs/data/${ key }/passive`);
 }
 
-function fetchChampTip( key, ally=true ) {
-	return retrieveDb(`/champs/data/${ key }/${ (ally) ? 'ally' : 'enemy' }tips`)
-		.then(tips => tips[ Math.floor( Math.random() * tips.length ) ]);
+function fetchChampTip( key, isAlly=true ) {
+	return retrieveDb(`/champs/data/${ key }/${ (isAlly) ? 'ally' : 'enemy' }tips`)
+		.then(tips => {
+			return {
+			Champion : key,
+			Tip : tips[ Math.floor( Math.random() * tips.length ) ]
+			}
+		});
 }
+
+/*let dummy_agent = {
+	idx: 0,
+	add: function(e) {
+		this[this.idx] = e;
+		this.idx++;
+	}
+};*/
