b67ecaa89b81fc8a36c3867daf4e9cfdb6ecab9c
Ethan David <ethan@eadd.ae>
2018-06-04 10:38:08 -0400

    realize made git mistake

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const { WebhookClient, Card, Suggestion } = require('dialogflow-fulfillment');
+const { dialogflow, BasicCard, SimpleResponse, Image } = require('actions-on-google');
 const firebase = require('firebase');
 const fbConfig = require('./nocommit/prof-ryze-firebase-usercfg.json');
 const version = '8.9.1';
@@ -31,69 +31,67 @@ firebase.initializeApp(fbConfig);
 
 let db = firebase.database();
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest( ( req, res ) => webHookDriver( req, res ) );
+app.intent('Ability-Desc', abilityDescHandler);
+app.intent('Ability-Cooldown', abilityCooldownHandler);
+app.intent('Ability-Scaling', abilityScalingHandler);
+app.intent('Passive-Desc', passiveDescHandler);
+app.intent('Champ-Tip-Ally', conv => champTipHandler( conv, true ) );
+app.intent('Champ-Tip-Enemy', conv => champTipHandler( conv, false ) );
 
-function webHookDriver( req, res ) {
-	let agent = new WebhookClient({request: req, response: res});
-	let intentMap = new Map();
-	intentMap.set('Ability-Desc', abilityDescHandler);
-	intentMap.set('Ability-Cooldown', abilityCooldownHandler);
-	intentMap.set('Ability-Scaling', abilityScalingHandler);
-	intentMap.set('Passive-Desc', passiveDescHandler);
-	intentMap.set('Champ-Tip-Ally', a => champTipHandler( a, true ) );
-	intentMap.set('Champ-Tip-Enemy', a => champTipHandler( a, false ) );
-	agent.handleRequest(intentMap);
-}
-
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
 
 // Ability-Cooldown
 
-function abilityCooldownHandler( agent ) {
-	let key = agent.parameters.Champion;
-	let idx = ABILITIES[ agent.parameters.Ability ];
+function abilityCooldownHandler( conv ) {
+	let key = conv.parameters.Champion;
+	let idx = ABILITIES[ conv.parameters.Ability ];
 	return fetchChampAbility( key, idx )
-		.then( ability => abilityCooldownResponses( agent, ability ) );
+		.then( ability => abilityCooldownResponses( conv, ability ) );
 }
 
-function abilityCooldownResponses( agent, ability ) {
+// TODO: migrate to table cards when out of dev preview
+// TODO: fix ultimate hunter text
+// (https://developers.google.com/actions/assistant/responses#table_card)
+function abilityCooldownResponses( conv, ability ) {
 	let fmtCDR = (x, cdr) => Math.floor(x * (1 - cdr));
-	// let isUltimate = ABILITIES[ agent.parameters.Ability ] === ABILITIES.R;
-	agent.add(
-		`${ ability.name } has a cooldown of between ${ ability.cooldown[0] } ` + 
-		`seconds at rank 1 to ${ ability.cooldown[ ability.cooldown.length-1 ] }` +
-		` seconds at rank ${ ability.cooldown.length }.` +
-		` With 45% cooldown reduction, it has a cooldown of ${ fmtCDR(ability.cooldown[0]) } seconds at rank 1` +
-		` to ${ fmtCDR(ability.cooldown[ ability.cooldown.length - 1 ]) } seconds at rank ${ ability.cooldown.length }.`);
-	let card = new Card( `${ability.name} Cooldown` );
 	let cds = ability.cooldown;
-	card.setImage( ddragon + '/img/spell/' + ability.image.full );
-	// TODO: migrate to table cards when out of dev preview
-	// TODO: fix ultimate hunter text
-	// (https://developers.google.com/actions/assistant/responses#table_card)
-	card.setText(
-		`0% CDR: ${ cds.join('/') }  \n
-		45% CDR: ${ cds.map(x => fmtCDR(x)).join('/') }`
-		/*+ (isUltimate) ? 
-		`0% CDR with Ultimate Hunter: ${ cds.map(x => (x * 0.85).toFixed(1)).join('/')}
-		45% CDR with Ultimate Hunter: ${ cds.map(x => (x * 0.40).toFixed(1)).join('/')}`
-		: ''*/
-	);
-	card.setPlatform(agent.requestSource);
-	agent.add(card);
+
+	let ttsText = `${ ability.name } has a cooldown of between ${ ability.cooldown[0] } ` + 
+			`seconds at rank 1 to ${ ability.cooldown[ ability.cooldown.length-1 ] }` +
+			` seconds at rank ${ ability.cooldown.length }.` +
+			` With 45% cooldown reduction, it has a cooldown of ${ fmtCDR(ability.cooldown[0]) } seconds at rank 1` +
+			` to ${ fmtCDR(ability.cooldown[ ability.cooldown.length - 1 ]) } seconds at rank ${ ability.cooldown.length }.`
+
+	let base_card = {
+		title : `${ability.name} Cooldown`,
+		image : {
+			url : ddragon + '/img/spell' + ability.image.full,
+			alt : ability.name
+		},
+		text : `0% CDR: ${ cds.join('/') }  \n
+				45% CDR: ${ cds.map(x => fmtCDR(x)).join('/') }`
+	};
+	conv.ask( new SimpleResponse(ttsText));
+	conv.ask( new BasicCard(base_card));
 }
 
 // Ability-Scaling
  
-function abilityScalingHandler( agent ) {
-	let key = agent.parameters.Champion;
-	let idx = ABILITIES[ agent.parameters.Ability ];
+function abilityScalingHandler( conv ) {
+	let key = conv.parameters.Champion;
+	let idx = ABILITIES[ conv.parameters.Ability ];
 	return fetchChampAbility( key, idx )
-		.then( ability => abilityScalingResponses( agent, ability ) );
+		.then( ability => abilityScalingResponses( conv, ability ) );
 }
 
-function abilityScalingResponses( agent, ability ) {
-	let card = new Card(`Scaling ${ ability.name }`);
-	card.setImage( ability.image.full );
+function abilityScalingResponses( conv, ability ) {	
+	let base_card = {
+		title : `Scaling ${ ability.name }`,
+		image : new Image({
+			url : ddragon + '/img/spell/' + ability.image.full,
+			alt : ability.name
+		})
+	};
 	// Add scaling factors to a set to ensure unique values
 	let scales = new Set([]);
 	for (let v of ability.vars) {
@@ -107,23 +105,22 @@ function abilityScalingResponses( agent, ability ) {
 	// "Ability Name scales with x."
 	// "Ability Name scales with x and y."
 	// "Ability Name scales with x, y, and z."
-	card.setText(`${ability.name} scales with ${
+	base_card.text = `${ability.name} scales with ${
 		scales.slice(0, scales.length - 1).join(', ')
 	}${ (scales.length > 1) ?
 		' and ' + scales[scales.length -1] :
 		scales[0]
-	}.`);
+	}.`;
 
-	card.setImage( ddragon + '/img/spell/' + ability.image.full);
-	card.setPlatform(agent.requestSource);
-	agent.add(card);
+	conv.ask(new SimpleResponse(base_card.text));
+	conv.ask(new BasicCard(base_card));
 }
 
 // Champ-Tip-Ally
 // Champ-Tip-Enemy
 
-function champTipHandler( agent, isAlly=true ) {
-	let key = agent.parameters.Champion;
+function champTipHandler( conv, isAlly=true ) {
+	let key = conv.parameters.Champion;
 	let fetchTip = Promise.all([fetchChampTips(key, isAlly), fetchChampImage(key)])
 		.then(results => {
 			let tips = results[0];
@@ -134,61 +131,55 @@ function champTipHandler( agent, isAlly=true ) {
 				Image : ddragon + '/img/champion/' + image
 			};
 		});
-	return fetchTip.then( tip => champTipResponses( agent, tip, isAlly ));
+	return fetchTip.then( tip => champTipResponses( conv, tip, isAlly ));
 }
 
 function fetchChampImage ( key ) {
 	return retrieveDb(`/champs/data/${key}/image/full`);
 }
-function champTipResponses ( agent, tip, isAlly ) {
+function champTipResponses ( conv, tip, isAlly ) {
 	let base_card = {
 		title : (isAlly) ? `Playing as ${tip.Champion}` : `Playing against ${tip.Champion}`,
 		text : tip.Tip,
-		imageUrl : tip.Image
+		image : new Image({
+			url : tip.Image,
+			alt : tip.Champion
+		})
 	};
-	let gcard = new Card(base_card);
-	gcard.setPlatform(agent.requestSource);
-	agent.add(gcard);
+	conv.ask(new SimpleResponse(base_card.text));
+	conv.ask(new BasicCard(base_card));
 }
 
 // Ability-Desc
 // Passive-Desc
 
-function abilityDescHandler ( agent ) {
-	let key = agent.parameters.Champion;
-	let idx = ABILITIES[ agent.parameters.Ability ];
+function abilityDescHandler ( conv ) {
+	let key = conv.parameters.Champion;
+	let idx = ABILITIES[ conv.parameters.Ability ];
 	return fetchChampAbility( key, idx )
-		.then(ability => abilityDescResponses(agent, ability));
+		.then(ability => abilityDescResponses(conv, ability));
 }
 
-function passiveDescHandler( agent ) {
-	let key = agent.parameters.Champion;
+function passiveDescHandler( conv ) {
+	let key = conv.parameters.Champion;
 	return fetchChampPassive( key )
-		.then(passive => passiveDescResponses(agent, passive));
+		.then(passive => passiveDescResponses(conv, passive));
 }
 
-function passiveDescResponses( agent, passive ) {
-	agent.add( `${ passive.name }: ${ passive.sanitizedDescription }` );
-	let base_card = {
-		title : passive.name,
-		text : passive.sanitizedDescription,
-		imageUrl : ddragon + '/img/passive/' + passive.image.full
-	};
-	let gcard = new Card(base_card);
-	gcard.setPlatform(agent.requestSource);
-	agent.add(gcard);
+function passiveDescResponses( conv, passive ) {
+	return abilityDescResponse(conv, passive);
 }
 
-function abilityDescResponses( agent, ability ) {
-	agent.add( `${ ability.name }: ${ ability.sanitizedDescription }` );
-	let base_card = {
+function abilityDescResponses( conv, ability ) {
+	conv.ask(new SimpleResponse(`${ ability.name }: ${ ability.sanitizedDescription }`));
+	conv.ask(new BasicCard({
 		title : ability.name,
 		text : ability.sanitizedDescription,
-		imageUrl : ddragon + '/img/spell/' + ability.image.full
-	};
-	let gcard = new Card(base_card);
-	gcard.setPlatform(agent.requestSource);
-	agent.add(gcard);
+		image : new Image({
+			url : ddragon + '/img/spell/' + ability.image.full,
+			alt : ability.name
+		})
+	}));
 }
 
 // DB Fetch
@@ -217,7 +208,7 @@ function fetchChampTips( key, isAlly=true ) {
 
 // debug code
 
-/*let dummy_agent = {
+/*let dummy_conv = {
 	idx: 0,
 	parameters : {
 		Champion: 'Jax',
